7ca89a1359c24bda930dbf5c41461723d8778e5d
Thanh <tnkchaseme@gmail.com>
2021-11-17 16:49:27 +0700

    update database url with os.environ

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from functools import wraps
 from flask import abort
 from forms import CreatePostForm, RegisterForm, LoginForm
 from flask_gravatar import Gravatar
-
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
@@ -19,7 +19,8 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # login manager
